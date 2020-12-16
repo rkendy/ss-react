c0ac754104840541e48eb31b2a4b2d4da85691f5
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-12-16 11:18:26 -0300

    Home com cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
-import MenuMain from "./MenuMain";
-import MenuFunctions from "./MenuFunctions";
+import { Row, Col, Card, Button } from "react-bootstrap";
+
 import axios from "axios";
+
+import MenuMain from "./MenuMain";
+import TabelaSolicitacoes from "./TabelaSolicitacoes";
 
 export default function Home() {
   return (
     <div>
-      <MenuMain />
-      <div className="container">Body</div>
+      <Row>
+        <Col md={6}>
+          <Card className="mb-4">
+            <Card.Header bg="primary">Minhas Solicitações</Card.Header>
+            <Card.Body>
+              <TabelaSolicitacoes />
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="mb-4">
+            <Card.Header>Novas Solicitações</Card.Header>
+            <Card.Body>
+              <TabelaSolicitacoes />
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
